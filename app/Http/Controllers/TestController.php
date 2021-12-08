4ba8957ff5fa8cc71855fde6ad23acef70b8633a
tatsuro1997 <minagawa@dptr.jp>
2021-12-08 20:27:52 +0900

    クエリビルダ

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,13 +6,20 @@ use Illuminate\Http\Request;
 
 use App\Models\Test;
 
+use Illuminate\Support\Facades\DB;
+
+
 class TestController extends Controller
 {
     //
     public function index(){
         $values = Test::all();
 
-        // dd($values);
+        $tests = DB::table('tests')
+                            ->select('id')
+                            ->get();
+
+        dd($tests);
 
         return view('tests.test', compact('values'));
     }
