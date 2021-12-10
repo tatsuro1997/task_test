3395783d5a999dee7e614741f7e49fa05be72949
tatsuro1997 <minagawa@dptr.jp>
2021-12-10 09:48:19 +0900

    add index.blade

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,13 @@ Route::get('/', function () {
 
 Route::get('tests/test', 'TestController@index');
 
+
+
+Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function(){
+    Route::get('index', 'ContactFormController@index')->name('contact.index');
+});
 // REST
-Route::resource('contacts', 'ContactFormController');
+// Route::resource('contacts', 'ContactFormController');
 
 Auth::routes();
 
