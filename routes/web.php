00469a5f2c1240e14488ba2675f6c62a4d305b52
tatsuro1997 <minagawa@dptr.jp>
2021-12-10 09:39:12 +0900

    make controller ContactFormController

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
 
 Route::get('tests/test', 'TestController@index');
 
+// REST
+Route::resource('contacts', 'ContactFormController');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
