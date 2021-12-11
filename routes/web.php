456bb9aee1ed3e14ca440afd4e95ccbdef1f1bc2
tatsuro1997 <minagawa@dptr.jp>
2021-12-11 10:51:03 +0900

    create show.blade

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function(){
     Route::get('index', 'ContactFormController@index')->name('contact.index');
     Route::get('create', 'ContactFormController@create')->name('contact.create');
     Route::post('store', 'ContactFormController@store')->name('contact.store');
+    Route::get('show/{id}', 'ContactFormController@show')->name('contact.show');
 });
 // REST
 // Route::resource('contacts', 'ContactFormController');
