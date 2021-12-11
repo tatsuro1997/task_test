287c86b7d636c6b65b590e07a073bec918057794
tatsuro1997 <minagawa@dptr.jp>
2021-12-11 16:45:34 +0900

    create search_form

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -16,17 +16,42 @@ class ContactFormController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+
+        $search = $request->input('search');
+        // dd($request);
+
         //エロくワント　ORマッパー
         // $contacts = ContactForm::all();
 
         // クエリビルダ
-        $contacts = DB::table('contact_forms')
-                                ->select('id', 'your_name', 'title', 'created_at')
-                                ->orderBy('created_at', 'desc')
-                                ->paginate(20);
+        // $contacts = DB::table('contact_forms')
+        //                         ->select('id', 'your_name', 'title', 'created_at')
+        //                         ->orderBy('created_at', 'desc')
+        //                         ->paginate(20);
         // dd($contacts);
+
+        // 検索フォーム用
+        $query = DB::table('contact_forms');
+
+        if($search !== null){
+            // 全角スペースを半角に
+            $search_split = mb_convert_kana($search, 's');
+
+            // 空白で区切る
+            $search_split2 = preg_split('/[\s]+/', $search_split, -1, PREG_SPLIT_NO_EMPTY);
+
+            foreach($search_split2 as $value)
+            {
+                $query->where('your_name', 'like', '%'.$value.'%');
+            }
+        };
+
+        $query->select('id', 'your_name', 'title', 'created_at');
+        $query->orderBy('created_at', 'asc');
+        $contacts = $query->paginate(20);
+
         return view('contact.index', compact('contacts'));
     }
 
